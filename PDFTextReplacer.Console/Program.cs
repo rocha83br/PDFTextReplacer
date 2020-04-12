87e39aa739de56b29ff9823db57544d8adfed94c
Renato Rocha <rocha83br@gmail.com>
2020-04-12 02:36:21 -0300

    Initial commit

---
+++ PDFTextReplacer.Console/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rochas.PDFText;
+using System.IO;
+
+namespace Rochas.PDFText.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sourceFile = string.Empty;
+            var destinationFile = string.Empty;
+            var replacements = new Dictionary<string, string>();
+            var pageNumber = 1;
+
+            System.Console.Clear();
+            System.Console.WriteLine("Rochas PDF Text Replacer");
+            System.Console.WriteLine("------------------------");
+
+            GetSourceFile(ref sourceFile);
+            GetDestinationFile(ref destinationFile);
+            GetReplacements(replacements);
+
+            try
+            {
+                System.Console.WriteLine();
+                PDFTextReplacer.ReplaceText(sourceFile, destinationFile, replacements, pageNumber);
+                System.Console.WriteLine("Done!");
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine($"Error while managing pdf document:{ex.Message}{Environment.NewLine}{ex.StackTrace}");
+            }
+            finally
+            {
+                System.Console.ReadLine();
+            }
+        }
+
+        static void GetSourceFile(ref string sourceFile)
+        {
+            while (!File.Exists(sourceFile))
+            {
+                System.Console.Write("Enter the source file path : ");
+                sourceFile = System.Console.ReadLine();
+
+                if (!File.Exists(sourceFile))
+                {
+                    System.Console.WriteLine("FILE NOT FOUND.");
+                    System.Console.WriteLine();
+                }
+                else
+                {
+                    System.Console.WriteLine("OK.");
+                    System.Console.WriteLine();
+                }
+            }
+        }
+
+        static void GetDestinationFile(ref string destinationFile)
+        {
+            var destinationPathExists = false;
+            while (!destinationPathExists)
+            {
+                System.Console.Write("Enter the destination file path : ");
+                destinationFile = System.Console.ReadLine();
+
+                destinationPathExists = !string.IsNullOrWhiteSpace(destinationFile)
+                                      && Directory.Exists(Path.GetDirectoryName(destinationFile));
+
+                if (!destinationPathExists)
+                {
+                    System.Console.WriteLine("PATH NOT FOUND.");
+                    System.Console.WriteLine();
+                }
+                else
+                {
+                    System.Console.WriteLine("OK.");
+                    System.Console.WriteLine();
+                }
+            }
+        }
+
+        static void GetReplacements(IDictionary<string, string> replacements)
+        {
+            var keyValue = string.Empty;
+            while (string.IsNullOrWhiteSpace(keyValue))
+            {
+                System.Console.WriteLine("Enter the replacement items, after type END to continue : ");
+                keyValue = System.Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(keyValue))
+                {
+                    if (keyValue.Contains("|"))
+                    {
+                        var keyValueArr = keyValue.Split('|');
+                        replacements.Add(keyValueArr[0], keyValueArr[1]);
+                    }
+                    else
+                    {
+                        keyValue = string.Empty;
+                        System.Console.WriteLine("INVALID REPLACEMENT FORMAT, USE: Source|Replacement FORMAT.");
+                        System.Console.WriteLine();
+                    }
+                }
+                else if (keyValue.ToUpper().Equals("END"))
+                {
+                    if (replacements.Any())
+                    {
+                        System.Console.WriteLine("OK.");
+                        System.Console.WriteLine();
+                    }
+                    else
+                        keyValue = string.Empty;
+                }
+            }
+        }
+
+        static void GetPageNumber(out int pageNumber)
+        {
+            System.Console.Write("Enter the page number to act : ");
+            var page = System.Console.ReadLine();
+
+            if (!int.TryParse(page, out pageNumber))
+            {
+                System.Console.WriteLine("INVALID PAGE NUMBER.");
+                System.Console.WriteLine();
+            }
+            else
+            {
+                System.Console.WriteLine("OK.");
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ PDFTextReplacer/PDFTextReplacer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using iText.Kernel.Pdf;
+
+namespace Rochas.PDFText
+{
+    public static class PDFTextReplacer
+    {
+        public static bool ReplaceText(string sourceFile, string destinationFile, Dictionary<string, string> replacements, int pageNumber = 1)
+        {
+            var result = false;
+
+            CheckArguments(sourceFile, destinationFile, replacements);
+
+            var pdfReader = new PdfReader(sourceFile);
+            var pdfWriter = new PdfWriter(destinationFile);
+            if ((pdfReader != null) && (pdfWriter != null))
+            {
+                var pdfDoc = new PdfDocument(pdfReader, pdfWriter);
+                if (pdfDoc != null)
+                {
+                    var pdfPage = pdfDoc.GetPage(pageNumber);
+                    var pdfDic = pdfPage.GetPdfObject();
+                    var pdfObject = pdfDic.Get(PdfName.Contents);
+                    var pdfContent = (PdfStream)pdfObject;
+                    var pdfData = pdfContent.GetBytes();
+                    var pdfStrData = Encoding.GetEncoding(1252).GetString(pdfData);
+
+                    foreach (var replace in replacements)
+                        pdfStrData = pdfStrData.Replace(replace.Key, replace.Value);
+
+                    pdfContent.SetData(Encoding.GetEncoding(1252).GetBytes(pdfStrData));                    
+                }
+
+                pdfWriter.Flush();
+
+                pdfDoc.Close();
+                pdfReader.Close();
+                pdfWriter.Close();
+
+                result = true;
+            }
+
+            return result;
+        }
+
+        private static void CheckArguments(string sourceFile, string destinationFile, Dictionary<string, string> replacements)
+        {
+            if (string.IsNullOrWhiteSpace(sourceFile))
+                throw new ArgumentNullException("Source file name argument is required.");
+
+            if (string.IsNullOrWhiteSpace(sourceFile))
+                throw new ArgumentNullException("Destination file name argument is required.");
+
+            if (replacements == null)
+                throw new ArgumentNullException("String replacements is required.");
+        }
+    }
+}
